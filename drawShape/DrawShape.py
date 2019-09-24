6def88192ea4c210dc67d3600ec10649d8e20f39
Joost Zonneveld <joostz14@gmail.com>
2019-09-24 17:48:47 +0200

    changes to drawshape

--- drawShape/DrawShape.py
+++ drawShape/DrawShape.py
@@ -1,24 +1,21 @@
-class DrawShape:
+def square(x):
+    for i in range(x):
+        output = ''
+        for j in range(x):
+            output += '*'
+        print(output)
 
-    @staticmethod
-    def square(x):
-        for i in range(x):
-            output = ''
-            for j in range(x):
+
+def empty_square(x):
+    for i in range(x):
+        output = ''
+        for j in range(x):
+            if j == 0 or j == x-1 or i == 0 or i == x-1:
                 output += '*'
-            print(output)
-
-    @staticmethod
-    def empty_square(x):
-        for i in range(x):
-            output = ''
-            for j in range(x):
-                if j == 0 or j == x-1 or i == 0 or i == x-1:
-                    output += '*'
-                else:
-                    output += ' '
-            print(output)
+            else:
+                output += ' '
+        print(output)
 
 
 def run():
-    DrawShape.empty_square(5)
+    square(5)
